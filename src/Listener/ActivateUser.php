1e67cdc6b205408182603f4ec4831d98fb4e2533
Maicol <maicolbattistini@live.it>
2020-09-28 16:59:21 +0200

    refactor: ♻️ Refactor code

--- src/Listener/ActivateUser.php
+++ src/Listener/ActivateUser.php
@@ -8,14 +8,14 @@ use Illuminate\Contracts\Events\Dispatcher;
 class ActivateUser
 {
     public function subscribe(Dispatcher $events): void
-	{
-		$events->listen(Registered::class, [$this, 'activateUser']);
-	}
+    {
+        $events->listen(Registered::class, [$this, 'activateUser']);
+    }
 
-	public function activateUser(Registered $event): void
-	{
-		$user = $event->user;
-		$user->activate();
-		$user->save();
-	}
+    public function activateUser(Registered $event): void
+    {
+        $user = $event->user;
+        $user->activate();
+        $user->save();
+    }
 }
