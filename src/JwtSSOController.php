8f1738d0a1b5791bafc297f9051e4707b716c7ae
Maicol <maicolbattistini@live.it>
2020-10-13 22:03:48 +0200

    fix: Exceptions

--- src/JwtSSOController.php
+++ src/JwtSSOController.php
@@ -6,14 +6,15 @@ use Exception;
 use Flarum\Api\Client;
 use Flarum\Api\Controller\CreateUserController;
 use Flarum\Foundation\Application;
+use Flarum\Http\AccessToken;
 use Flarum\Http\Rememberer;
 use Flarum\Http\SessionAuthenticator;
 use Flarum\Settings\SettingsRepositoryInterface;
 use Flarum\User\Exception\PermissionDeniedException;
 use Flarum\User\User;
 use Flarum\User\UserRepository;
-use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
+use InvalidArgumentException;
 use Laminas\Diactoros\Response\JsonResponse;
 use Lcobucci\JWT\Parser;
 use Lcobucci\JWT\Signer\Hmac\Sha256;
@@ -88,70 +89,95 @@ class JwtSSOController implements RequestHandlerInterface
      */
     public function handle(Request $request): ResponseInterface
     {
+        // Get token
         $headers = $request->getHeader('Authorization');
-        $token = (new Parser())->parse(Str::after(preg_grep('/^Bearer\s\w+$/', $headers)[0], 'Bearer '));
+        if (empty($headers)) {
+            http_response_code(400);
+            throw new InvalidArgumentException("No Authorization header was set");
+        }
+
+        /** @noinspection RegExpRedundantEscape */
+        $header = preg_grep('/^Bearer\s[A-Za-z0-9\-_\=]+\.[A-Za-z0-9\-_\=]+\.?[A-Za-z0-9\-_.+\/\=]*$/', explode(', ', $headers[0]));
+        if (empty($header)) {
+            http_response_code(400);
+            throw new InvalidArgumentException("No JWT found in Authorization headers");
+        }
+
+        $jwt = Str::after($header[0], 'Bearer ');
+        $token = (new Parser())->parse($jwt);
+
+        // Validate token
         if (!$token->verify(new Sha256(), new Key($this->signer_key))) {
             throw new PermissionDeniedException('Signature key does not correspond to the one on the token!');
         }
+        // Signup
+        $jti = $token->getClaim('jti');
 
+        $data = new ValidationData(); // It will use the current time to validate (iat, nbf and exp)
+        $data->setIssuer($this->iss);
+        $data->setAudience($this->site_url);
+        $data->setId($jti);
+
+        if (!$token->validate($data)) {
+            if ($token->isExpired()) {
+                $message = 'Token expired.';
+            }
+
+            throw new PermissionDeniedException($message ?? 'Invalid token.');
+        }
+
+        // ↓↓ Added for IDE autocompletion
+        /** @var \Maicol07\SSO\User $jwt_user */
         $jwt_user = $token->getClaim('user');
 
         // remove any sizing params
-        $avatar = Arr::get($jwt_user, 'attributes.avatarUrl');
+        $avatar = $jwt_user->attributes->avatarUrl;
         $param = '?sz=';
         if (strpos($avatar, $param)) {
             $avatar = substr($avatar, 0, strpos($avatar, $param));
         }
 
-        $user = $this->users->findByIdentification(Arr::get($jwt_user, 'attributes.email') ?? Arr::get($jwt_user, 'attributes.username'));
+        $user = $this->users->findByIdentification($jwt_user->attributes->email ?? $jwt_user->attributes->username);
 
         if ($user !== null) {
             $token = $this->login($user, $avatar, $request);
         } else {
-            // Signup
-            $jti = $token->getClaim('jti');
-
-            $data = new ValidationData(); // It will use the current time to validate (iat, nbf and exp)
-            $data->setIssuer($this->iss);
-            $data->setAudience($this->site_url);
-            $data->setId($jti);
-
-            if (!$token->validate($data)) {
-                if ($token->isExpired()) {
-                    $message = 'Token expired.';
-                }
-
-                throw new PermissionDeniedException($message ?? 'Invalid token.');
-            }
-
-            $jwt_user['attributes']['isEmailConfirmed'] = true;
-
-            $controller = CreateUserController::class;
+            /** @noinspection PhpUndefinedFieldInspection */
+            $jwt_user->attributes->isEmailConfirmed = true;
 
             $actor = $this->users->findOrFail(1);
-            $body = ['data' => $jwt_user];
+            $body = [
+                'data' => [
+                    'attributes' => (array)$jwt_user->attributes,
+                    'relationships' => (array)$jwt_user->relationships
+                ]
+            ];
 
             try {
-                $response = $this->api->send($controller, $actor, [], $body);
+                $response = $this->api->send(CreateUserController::class, $actor, [], $body);
             } catch (Exception $e) {
-                throw new RuntimeException("Error during signup");
+                throw new RuntimeException("Error during signup.");
             }
 
             $body = json_decode($response->getBody(), false);
-            if (isset($body->data)) {
-                $id = $body->data->id;
-                $user = $this->users->findOrFail($id);
-                $user->activate();
-                $user->save();
-
-                $token = $this->login($user, $avatar, $request);
+            if (empty($body->data)) {
+                throw new RuntimeException("Signup failed.");
             }
+            $id = $body->data->id;
+            $user = $this->users->findOrFail($id);
+            $user->activate();
+            $user->save();
+
+            $token = $this->login($user, $avatar, $request);
         }
 
-        return new JsonResponse($token);
+        return new JsonResponse([
+            'token' => $token->token,
+            'userId' => $user->id
+        ]);
     }
 
-    private function login(User $user, string $avatar, Request $request, ?ResponseInterface $response = null)
+    private function login(User $user, ?string $avatar, Request $request, ?ResponseInterface $response = null): AccessToken
     {
         // Login
         $id = $user->id;
@@ -161,7 +187,8 @@ class JwtSSOController implements RequestHandlerInterface
 
         $session = $request->getAttribute('session');
         $this->authenticator->logIn($session, $id);
-        $token = $user->accessTokens()->latest()->getResults();
+        $token = AccessToken::generate($user->id);
+        $token->save();
 
         if ($response !== null) {
             $this->rememberer->rememberUser($response, $id);
