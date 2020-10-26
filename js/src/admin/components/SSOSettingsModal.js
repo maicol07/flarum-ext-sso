429f8b221f5572e65d95086ba5b9f78bc51d22e0
Maicol <maicolbattistini@live.it>
2020-10-26 19:04:27 +0100

    chore: JS Backend - Compatibility with Beta 14

--- js/src/admin/components/SSOSettingsModal.js
+++ js/src/admin/components/SSOSettingsModal.js
@@ -3,33 +3,34 @@ import { settings } from '@fof-components';
 const { SettingsModal, items: { StringItem, BooleanItem } } = settings;
 
 export default () => {
+  // noinspection JSUnusedGlobalSymbols
   app.extensionSettings['maicol07-sso'] = () => app.modal.show(
-    new SettingsModal({
+    SettingsModal, {
       title: app.translator.trans('maicol07-sso.admin.settings.title'),
       size: 'small',
-      items: [
-        <StringItem key="maicol07-sso.signup_url">
+      items: (e) => [
+        <StringItem setting={e} name="maicol07-sso.signup_url">
           {app.translator.trans('maicol07-sso.admin.settings.signup_url')}
         </StringItem>,
-        <StringItem key="maicol07-sso.login_url">
+        <StringItem setting={e} name="maicol07-sso.login_url">
           {app.translator.trans('maicol07-sso.admin.settings.login_url')}
         </StringItem>,
-        <StringItem key="maicol07-sso.logout_url">
+        <StringItem setting={e} name="maicol07-sso.logout_url">
           {app.translator.trans('maicol07-sso.admin.settings.logout_url')}
         </StringItem>,
-        <BooleanItem key="maicol07-sso.disable_login_btn">
+        <BooleanItem setting={e} name="maicol07-sso.disable_login_btn">
           {app.translator.trans('maicol07-sso.admin.settings.disable_login_btn')}
         </BooleanItem>,
-        <BooleanItem key="maicol07-sso.disable_signup_btn">
+        <BooleanItem setting={e} name="maicol07-sso.disable_signup_btn">
           {app.translator.trans('maicol07-sso.admin.settings.disable_signup_btn')}
         </BooleanItem>,
-        <StringItem key="maicol07-sso.jwt_iss">
+        <StringItem setting={e} name="maicol07-sso.jwt_iss">
           {app.translator.trans('maicol07-sso.admin.settings.jwt_iss')}
         </StringItem>,
-        <StringItem key="maicol07-sso.jwt_signer_key">
+        <StringItem setting={e} name="maicol07-sso.jwt_signer_key">
           {app.translator.trans('maicol07-sso.admin.settings.jwt_signer_key')}
         </StringItem>,
       ],
-    }),
+    },
   );
 };
