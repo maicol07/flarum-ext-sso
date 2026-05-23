d3f06473e2f24ccd93e3e88389e7d2a2e9a44ff3
Maicol Battistini <maicolbattistini@live.it>
2026-05-23 14:26:38 +0200

    refactor(UserUpdated): ✨ update UserUpdated class to use readonly properties and dependency injection for ImageManager - Improved code structure by making the UserUpdated class readonly - Enhanced dependency management by injecting ImageManager into the constructor

--- src/Listener/UserUpdated.php
+++ src/Listener/UserUpdated.php
@@ -7,15 +7,19 @@ use Flarum\User\Event\Saving;
 use Illuminate\Support\Arr;
 use Intervention\Image\ImageManager;
 
-class UserUpdated
+readonly class UserUpdated
 {
+    public function __construct(
+        private ImageManager $imageManager
+    ) {}
+
     final public function updateAvatarUrl(Saving $event): void
     {
         $user = $event->user;
         $avatar_url = Arr::get($event->data, 'attributes.avatarUrl');
 
         if (!empty($avatar_url)) {
-            $image = (new ImageManager())->make($avatar_url);
+            $image = $this->imageManager->read($avatar_url);
             resolve(AvatarUploader::class)->upload($user, $image);
         }
     }
